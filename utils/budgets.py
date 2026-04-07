023164a78c871455bae131a865caf81d583d904d
Giacomo Boschi <giacomo.boschi@studenti.unipr.it>
2026-04-07 17:32:21 +0200

    Added materials budget logic

--- utils/budgets.py
+++ utils/budgets.py
@@ -37,7 +37,43 @@ def compute_production_budget(budget_df, inv_df, handle_missing=False):
             - production_df["beginning_inv"]
         )
 
+        # else: handle_missing=True is a planned extension. Not yet implemented
+
     return production_df
+
+
+def compute_materials_budget(production_df, raw_mat_df, handle_missing=False):
+    materials_df = production_df.merge(raw_mat_df, on="product", how="left")
+
+    materials_df["materials_for_production"] = (
+        materials_df["total_production"] * materials_df["material_kg_per_unit"]
+    )
+
+    if not handle_missing:
+        materials_df["ending_material_inventory"] = (
+            materials_df.groupby("product")["materials_for_production"].shift(-1)
+            * materials_df["raw_mat_start_inv"]
+        )
+
+    # else: handle_missing=True is yet to be implemented like in the above case
+
+    materials_df["beginning_materials_inventory"] = (
+        materials_df.groupby("product")["materials_for_production"]
+        * materials_df["raw_mat_start_inv"]
+    )
+
+    materials_df["materials_needs"] = (
+        materials_df["materials_for_production"]
+        + materials_df["ending_material_inventory"]
+    )
+
+    materials_df["materials_purchases"] = (
+        materials_df["materials_needs"] - materials_df["beginning_materials_inventory"]
+    )
+
+    return materials_df
+
+def total_materials()
 
 
 def compute_budgets(sales_payload, params_payload, handle_missing=False):
@@ -62,3 +98,23 @@ def compute_budgets(sales_payload, params_payload, handle_missing=False):
     )
 
     production_df = compute_production_budget(budget_df, inv_df, handle_missing)
+
+    raw_mat_inv = params["raw_materials_inventory"]["ending_inventory_rate"]
+
+    raw_mat_df = pd.DataFrame(
+        [
+            {
+                "product": code,
+                "material_cost_per_kg": product["material_cost_per_kg"],
+                "material_kg_per_unit": product["kg_per_unit"],
+            }
+            for code, product in params["product"].items()
+        ]
+    )
+    raw_mat_df.loc[:]["raw_mat_start_inv"] = raw_mat_inv
+
+    materials_df = compute_materials_budget(
+        production_df, raw_mat_df, handle_missing=False
+    )
+
+    return
